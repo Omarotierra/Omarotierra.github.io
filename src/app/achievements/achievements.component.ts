a7f396afa8725c0a032ecacedd7379780295ec90
Omarotierra <2omarhernandez2@gmail.com>
2023-09-15 09:48:53 -0600

    skills solucionado

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {  AchievementsService } from '../services/achievements.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
   styleUrls: ['./achievements.component.css']
 })
-export class AchievementsComponent implements OnInit {
-
-  constructor(private achievementsService: AchievementsService) { }
-
-  logros: any[] = [];
-
-  ngOnInit(): void {
-    this.achievementsService.getAchievements().subscribe((data: any) => {
-      console.log(data);
-      alert(data);
-      this.logros = data.achievements;
-    }
-    );
-  }
+export class AchievementsComponent {
 
 }
